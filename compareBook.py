045c68c5cc23ea9610f0c279663b407983258ded
Koichiro <tokails188s@gmail.com>
2025-05-11 23:53:08 +0900

    ヤフーフリマを追加

--- compareBook.py
+++ compareBook.py
@@ -33,6 +33,16 @@ def open_opac_search():
     search_url = f"https://opac.library.osaka-u.ac.jp/opac/opac_search/?lang=0&amode=2&appname=Netscape&version=5&cmode=0&smode=0&kywd={encoded_name}&index_amazon_s=Books&node_s="
     webbrowser.open(search_url)
 
+def open_yahoo_search():
+    product_name = entry.get()
+    if not product_name.strip():
+        messagebox.showerror("エラー", "商品名を入力してください")
+        return
+    # 商品名をURLエンコードして検索URL作成
+    encoded_name = urllib.parse.quote(product_name)
+    search_url = f"https://paypayfleamarket.yahoo.co.jp/search/{encoded_name}?open=1"
+    webbrowser.open(search_url)
+
 # GUI構築
 root = tk.Tk()
 root.title("商品検索オープナー")
@@ -49,6 +59,9 @@ amazon_search_button.pack(pady=10)
 
 mercari_search_button = tk.Button(root, text="メルカリで検索ページを開く", command=open_mercali_search)
 mercari_search_button.pack(pady=10)
+
+yahoo_search_button = tk.Button(root, text="ヤフーフリマで検索ページを開く", command=open_yahoo_search)
+yahoo_search_button.pack(pady=10)
 
 opac_search_button = tk.Button(root, text="OPACで検索ページを開く", command=open_opac_search)
 opac_search_button.pack(pady=10)
